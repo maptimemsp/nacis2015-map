455c3b8d4e10bf97b8bcb973d1c3499fdccc0946
Philippe Le Grand <Grandnord4reg@gmail.com>
2015-08-20 00:00:24 -0500

    Revert "Merge remote-tracking branch 'MaptimeMSP/master'"
    This reverts commit b6575feb8c54f4dd9d3f0ccc1941e23adb46e891, reversing
    changes made to a20041bd2c48437a65354db596f44e8fc7946327.

--- js/script.js
+++ js/script.js
@@ -16,89 +16,81 @@ var baselayer = new L.tileLayer('https://{s}.tiles.mapbox.com/v4/flatlandmaps.bf
 baselayer.addTo(map);
 
 //POI Icons
-<<<<<<< HEAD
 var poiPins={};
 poiPins['conf'] = L.MakiMarkers.icon({
-=======
-var confPin = L.MakiMarkers.icon({
->>>>>>> MaptimeMSP/master
     icon: "star",
     color: "#ff0000",
     size: "l"
 });
 
-var beerPin = L.MakiMarkers.icon({
+poiPins['beer'] = L.MakiMarkers.icon({
     icon: "beer",
     color: "#23344c",
     size: "m"
 });
 
-var barPin = L.MakiMarkers.icon({
+poiPins['bar'] = L.MakiMarkers.icon({
     icon: "bar",
     color: "#23344c",
     size: "m"
 });
 
-var restaurantPin = L.MakiMarkers.icon({
+poiPins['restaurant'] = L.MakiMarkers.icon({
     icon: "restaurant",
     color: "#23344c",
     size: "m"
 });
 
-var cafePin = L.MakiMarkers.icon({
+poiPins['cafe'] = L.MakiMarkers.icon({
     icon: "cafe",
     color: "#23344c",
     size: "m"
 });
 
-var hotelPin = L.MakiMarkers.icon({
+poiPins['hotel'] = L.MakiMarkers.icon({
 	icon: "lodging",
 	color: "#23344c",
 	size: "m"
 });
 
-var airportPin = L.MakiMarkers.icon({
+poiPins['airport'] = L.MakiMarkers.icon({
 	icon: "airport",
 	color: "#23344c",
 	size: "m"
 });
 
-<<<<<<< HEAD
 poiPins['ice-cream'] = L.MakiMarkers.icon({
-=======
-var icecreamPin = L.MakiMarkers.icon({
->>>>>>> MaptimeMSP/master
 	icon: "ice-cream",
 	color: "#23344c",
 	size: "m"
 });
 
-var museumPin = L.MakiMarkers.icon({
+poiPins['museum'] = L.MakiMarkers.icon({
 	icon: "museum",
 	color: "#23344c",
 	size: "m"
 });
 
-var parkPin = L.MakiMarkers.icon({
+poiPins['park'] = L.MakiMarkers.icon({
 	icon: "park",
 	color: "#23344c",
 	size: "m"
 });
 
-var theatrePin = L.MakiMarkers.icon({
+poiPins['theatre'] = L.MakiMarkers.icon({
 	icon: "theatre",
 	color: "#23344c",
 	size: "m"
 });
 
-var shopPin = L.MakiMarkers.icon({
+poiPins['shop'] = L.MakiMarkers.icon({
 	icon: "shop",
 	color: "#23344c",
 	size: "m"
 });
 
 //Nice Ride Icon
-var niceRidePin = L.MakiMarkers.icon({
+poiPins['niceRide'] = L.MakiMarkers.icon({
 	icon: "bicycle",
 	color: "#A8CF38",
 	size: "s"
@@ -117,52 +109,25 @@ var defaultPin = L.MakiMarkers.icon({
 });
 
 //LRT Station Icons
-<<<<<<< HEAD
 lrtPins={};
 lrtPins['blueLRT'] = L.MakiMarkers.icon({
-=======
-var blueLRTPin = L.MakiMarkers.icon({
->>>>>>> MaptimeMSP/master
 	icon: "rail-light",
 	color: "#0000FF",
 	size: "s"
 });
 
-<<<<<<< HEAD
 lrtPins['greenLRT'] = L.MakiMarkers.icon({
-=======
-var greenLRTPin = L.MakiMarkers.icon({
->>>>>>> MaptimeMSP/master
 	icon: "rail-light",
 	color: "#008000",
 	size: "s"
 });
 
-<<<<<<< HEAD
 lrtPins['rail'] = L.MakiMarkers.icon({
-=======
-var railPin = L.MakiMarkers.icon({
->>>>>>> MaptimeMSP/master
 	icon: "rail",
 	color: "#FFA500",
 	size: "s"
 });
 
-<<<<<<< HEAD
-=======
-var cinemaPin = L.MakiMarkers.icon({
-	icon: "cinema",
-	color: "#23344c",
-	size: "m"
-});
-
-var defaultPin = L.MakiMarkers.icon({
-	icon: "marker",
-	color: "#000000",
-	size: "s"
-});
-
->>>>>>> MaptimeMSP/master
 //Fun Run Route layer
 var funRunRoute = new L.GeoJSON.AJAX("js/transit/funRun.json",{
 	style: function (feature) {
@@ -272,50 +237,10 @@ var poiLayer = new L.GeoJSON.AJAX("js/places.geojson",{
 
       var marker = new L.marker(latlng);
       marker.setIcon(defaultPin);
-<<<<<<< HEAD
       if (feature.properties.poi_type in poiPins) {
         marker.setIcon(poiPins[feature.properties.poi_type]);
       }
       
-=======
-	if (feature.properties.poi_type === 'conf') {
-          marker.setIcon(confPin);
-         }
-      	if (feature.properties.poi_type === 'beer') {
-          marker.setIcon(beerPin);
-         }
-      	if (feature.properties.poi_type === 'bar') {
-          marker.setIcon(barPin);
-         }
-      	if (feature.properties.poi_type === 'restaurant') {
-          marker.setIcon(restaurantPin);
-         }
-      	if (feature.properties.poi_type === 'cafe') {
-          marker.setIcon(cafePin);
-         }
-      	if (feature.properties.poi_type === 'hotel') {
-          marker.setIcon(hotelPin);
-         }
-      	if (feature.properties.poi_type === 'airport') {
-          marker.setIcon(airportPin);
-         }
-      	if (feature.properties.poi_type === 'ice-cream') {
-    	  marker.setIcon(icecreamPin);
-      	 }
-      if (feature.properties.poi_type === 'museum') {
-          marker.setIcon(museumPin);
-         }
-      	if (feature.properties.poi_type === 'park') {
-          marker.setIcon(parkPin);
-         }
-      	if (feature.properties.poi_type === 'theatre') {
-      	  marker.setIcon(theatrePin);
-      	 }
-      	if (feature.properties.poi_type === 'shop') {
-      	  marker.setIcon(shopPin);
-      	 }
-	
->>>>>>> MaptimeMSP/master
 	marker.bindPopup(popup);
 
 		  return marker;
